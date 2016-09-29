b678e5a471c743de3f481e9de498cde4aed51a7b
snovits1 <snovits1@binghamton.edu>
2016-09-29 19:50:18 -0400

    fileproccessor update

--- charles_digiovanna_steven_novitske_assign2/charles_digiovanna_steven_novitske/registrationScheduler/src/registrationScheduler/util/FileProcessor.java
+++ charles_digiovanna_steven_novitske_assign2/charles_digiovanna_steven_novitske/registrationScheduler/src/registrationScheduler/util/FileProcessor.java
@@ -1,30 +1,38 @@
-package registrationScheduler.util;
+package registrationScheduler;//.util;
 
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-import registrationScheduler.util.Logger;
-
 public class FileProcessor {
-    String fileName;
+    FileReader reader;
+    BufferedReader buffReader;
 
     public FileProcessor(String file) {
-        Logger.writeMessage("FileProcessor constructed\n",
-                            Logger.DebugLevel.CONSTRUCTOR);
-        fileName = file;
+      Logger.writeMessage("FileProcessor constructed\n", Logger.DebugLevel.CONSTRUCTOR);
+      try {
+        reader = new FileReader(file);
+        buffReader = new BufferedReader(reader);
+      } catch(IOException ex) {
+        ex.printStackTrace();
+      }
     }
 
     public String readLine() {
-        String line = "";
-        try {
-            BufferedReader buffReader = new BufferedReader(new FileReader(fileName));
-            line = buffReader.readLine();
-            buffReader.close();
-        } catch(IOException ex) {
-            ex.printStackTrace();
-        }
+      String line = "";
+      try {
+        line = buffReader.readLine();
+      } catch(IOException ex) {
+        ex.printStackTrace();
+      }
+      return line;
+    }
 
-        return line;
+    public void finish() {
+      try {
+        buffReader.close();
+      } catch(IOException ex) {
+        ex.printStackTrace();
+      }
     }
 }
